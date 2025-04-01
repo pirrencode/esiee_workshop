70e3fdcf3c420dd661430fc00665eb6fcff440c0
Aleksey Vesyoliy <43518301+pirrencode@users.noreply.github.com>
2025-04-01 21:27:40 +0300

    Increased complexity of evaluation

--- app.py
+++ app.py
@@ -29,5 +29,13 @@ def compareFunc (x , y):
     
 print(compareFunc(300,200))
 
+# 2. Write IF ELSE statement to validate if x is larger than y and less than b. Return TRUE if YES
+
+def compareFunc2 (x , y, b):
+  if (x > y) and (x < b):
+    return True
+  return False
+
+print(compareFunc2(300,200,600))
 
 
