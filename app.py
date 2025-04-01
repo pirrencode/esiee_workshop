3ab16c7b4101ba54f0e6860f8e5878df52349a87
Aleksey Vesyoliy <43518301+pirrencode@users.noreply.github.com>
2025-04-01 21:04:10 +0300

    Created basis for our first application
    It includes mathematical operations and manipulations with data type

---
+++ app.py
@@ -0,0 +1,22 @@
+# Simple Math
+
+x = 2
+y = 3
+z = x + y
+
+print(x)
+print(y)
+
+a = 10
+b = 5
+c = a / b
+d = a * b
+e = a % b
+
+name = "Anna"
+surname = "Grey"
+
+student_name = name + " " + surname
+print(student_name)
+
+
