c34e48dc12fee0224b81842386b1631d5564220b
Aleksey Vesyoliy <43518301+pirrencode@users.noreply.github.com>
2025-04-08 20:47:26 +0300

    Bunch of tasks for 08.01.2025

--- tasks/tasks.py
+++ tasks/tasks.py
@@ -1,1 +1,8 @@
-# Practical problems to solve
+#Task 1: Create a function to cook your favorite dish. It should contain parameters, methods and conditions.
+
+#Task 2: Conditional Statements
+#Write a Python program that checks if a given number is positive, negative, or zero. Print an appropriate message for each case.
+                                                                                                                 
+#Task 3:
+#Write a program that prints all even numbers between 1 and 20 using both a for loop and a while loop.
+
