98562a6ef675349b828a54f48043f095db88f6f2
Aleksey Vesyoliy <43518301+pirrencode@users.noreply.github.com>
2025-05-28 20:56:00 +0300

    Update data_science.py

--- tasks/data_science.py
+++ tasks/data_science.py
@@ -7,3 +7,23 @@ import numpy as np
 x = np.arange(15, dtype=np.int64).reshape(3, 5)
 x[1:, ::2] = -99
 x
+
+x.max(axis=1)
+# array([ 4,  8, 13])
+
+# Generate normally distributed random numbers:
+rng = np.random.default_rng()
+
+samples = rng.normal(size=250000)
+samples
+
+# Create arrays
+array_1 = np.array([1, 2, 3])
+array_2 = np.array([4, 5, 6])
+
+# Perform element-wise operations
+dot_product = np.dot(array_1, array_2)  # Dot product
+elementwise_sum = array_1 + array_2  # Element-wise addition
+squared = np.square(array_1)  # Element-wise squaring
+
+print(dot_product, elementwise_sum, squared)
