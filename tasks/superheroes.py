5959840c3dfa651697addae25bf9676cdd3515b4
tomas-code-hub <tomas.campome@gmail.com>
2026-04-23 21:21:17 +0300

    Add new power, new alignment attribute and new superhero

--- tasks/superheroes.py
+++ tasks/superheroes.py
@@ -2,13 +2,14 @@
 
 
 class Superhero:
-    def __init__(self, name, powers, origin, friends, age):
+    def __init__(self, name, powers, origin, friends, age, alignment):
         self.name = name
         self.powers = list(powers)
         self.origin = origin
         self.friends = friends
         self.age = age
         self.energy = 100
+        self.alignment = alignment 
 
     def print_name(self):
         print(self.name)
@@ -20,6 +21,7 @@ class Superhero:
         print(self.origin)
         print(self.friends)
         print(self.age)
+        print(self.alignment) 
 
     def add_power(self, power):
         if power not in self.powers:
@@ -53,11 +55,45 @@ class Superhero:
             self.add_power("Shoot Lasers")
             self.energy -= 20
             print(f"{self.name} shoots lasers from eyes")
+#New power
+    def heal(self):
+        self.add_power("Healing")
+        self.energy += 30
+        if self.energy > 100:
+            self.energy = 100
+        print(f"{self.name} heals and restores energy")
+
+    def super_speed(self):
+        if self.energy >= 15:
+            self.add_power("Super Speed")
+            self.energy -= 15
+            print(f"{self.name} moves at incredible speed")
+
+#New parameter
+        def show_alignment(self):
+        if self.alignment == 0:
+            print("It is a good superhero")
+        else:
+            print("It is a bad superhero")
 
 
 # Example usage
 if __name__ == "__main__":
-    ironman = Superhero("IronMan", ["tech"], "Earth", "Pepper", 35)
+    ironman = Superhero("IronMan", ["tech"], "Earth", "Pepper", 35, 0)
     ironman.print_name()
     ironman.print_attributes()
     ironman.fly()
+    ironman.show_alignment()
+
+#New heros
+    superman = Superhero("Superman", ["heal"], "New York", "Pepper", 35, 0)
+    superman.print_name()
+    superman.print_attributes()
+    superman.fly()
+    superman.show_alignment()
+
+    loki = Superhero("Loki", ["super_speed"], "UK", "Nobody", 35, 1)
+    loki.print_name()
+    loki.print_attributes()
+    loki.fly()
+    loki.show_alignment()
