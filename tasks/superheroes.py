6659aecf680990ec4496e3b8277d4587e42058ef
amirrebeval <jnvgvvb@gmail.com>
2026-04-23 20:12:38 +0200

    Update superheroes.py

--- tasks/superheroes.py
+++ tasks/superheroes.py
@@ -2,12 +2,13 @@
 
 
 class Superhero:
-    def __init__(self, name, powers, origin, friends, age):
+    def _init_(self, name, powers, origin, friends, age, alignment):
         self.name = name
         self.powers = list(powers)
         self.origin = origin
         self.friends = friends
         self.age = age
+        self.alignment = alignment  # "good" or "evil"
         self.energy = 100
 
     def print_name(self):
@@ -15,49 +16,93 @@ class Superhero:
 
     def print_attributes(self):
         print("Superhero attributes are:")
-        print(self.name)
-        print(self.powers)
-        print(self.origin)
-        print(self.friends)
-        print(self.age)
+        print("Name:", self.name)
+        print("Powers:", self.powers)
+        print("Origin:", self.origin)
+        print("Friends:", self.friends)
+        print("Age:", self.age)
+        print("Alignment:", self.alignment)
+        print("Energy:", self.energy)
 
     def add_power(self, power):
         if power not in self.powers:
             self.powers.append(power)
 
     def fly(self):
-        self.add_power("Flight")
-        self.energy -= 50
-        print(f"{self.name} takes off into the sky")
+        if self.energy >= 50:
+            self.add_power("Flight")
+            self.energy -= 50
+            print(f"{self.name} takes off into the sky")
+        else:
+            print(f"{self.name} does not have enough energy to fly")
 
     def turn_invisible(self):
         if self.energy >= 1:
             self.add_power("Invisibility")
             self.energy -= 1
             print(f"{self.name} becomes completely invisible")
+        else:
+            print(f"{self.name} does not have enough energy")
 
     def super_strength(self):
         if self.energy >= 10:
             self.add_power("Super Strength")
             self.energy -= 10
             print(f"{self.name} becomes strong")
+        else:
+            print(f"{self.name} does not have enough energy")
 
     def teleportation(self):
         if self.energy >= 10:
             self.add_power("Teleportation")
             self.energy -= 10
             print(f"{self.name} instantly teleports")
+        else:
+            print(f"{self.name} does not have enough energy")
 
     def shoot_lasers(self):
         if self.energy >= 20:
             self.add_power("Shoot Lasers")
             self.energy -= 20
             print(f"{self.name} shoots lasers from eyes")
+        else:
+            print(f"{self.name} does not have enough energy")
+
+    def heal(self):
+        if self.energy >= 15:
+            self.add_power("Healing")
+            self.energy -= 15
+            print(f"{self.name} uses healing powers")
+        else:
+            print(f"{self.name} does not have enough energy")
+
+    def control_thunder(self):
+        if self.energy >= 25:
+            self.add_power("Thunder Control")
+            self.energy -= 25
+            print(f"{self.name} summons thunder and lightning")
+        else:
+            print(f"{self.name} does not have enough energy")
 
 
 # Example usage
-if __name__ == "__main__":
-    ironman = Superhero("IronMan", ["tech"], "Earth", "Pepper", 35)
+if _name_ == "_main_":
+    ironman = Superhero("IronMan", ["Tech"], "Earth", "Pepper", 35, "good")
+    thor = Superhero("Thor", ["Hammer", "Strength"], "Asgard", "Loki", 1500, "good")
+    loki = Superhero("Loki", ["Magic", "Illusion"], "Asgard", "Thor", 1050, "evil")
+
     ironman.print_name()
     ironman.print_attributes()
     ironman.fly()
+    print()
+
+    thor.print_name()
+    thor.print_attributes()
+    thor.control_thunder()
+    thor.fly()
+    print()
+
+    loki.print_name()
+    loki.print_attributes()
+    loki.turn_invisible()
+    loki.teleportation()
