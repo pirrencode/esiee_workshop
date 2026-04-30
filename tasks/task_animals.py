7bd650b8e78d58e43bc44b210a3edebe5de7ae1a
Aleksey Vesyoliy <43518301+pirrencode@users.noreply.github.com>
2026-04-30 21:01:57 +0300

    Static method sample

--- tasks/task_animals.py
+++ tasks/task_animals.py
@@ -5,6 +5,20 @@
 # Instantiate each and call make_sound()
 ##################
 
+# BONUS: Add static method
+
+@staticmethod
+def createAnimal(animal, race):
+    if animal == "Dog":
+        return Dog(race)
+    elif animal == "Cat":
+        return Cat(race)
+    elif animal == "Bird":
+        return Bird(race)
+    else:
+        print("Sorry I dont know this animal")
+
+
 class Animal:
     def __init__(self,race,sound):
         self.race = race
@@ -29,7 +43,12 @@ dog1 = Dog("Oslo")
 cat1 = Cat("Aziza")
 bird1 = Bird("Coco")
 
+dog2 = createAnimal("Dog", "Oslo")
+bird2 = createAnimal("Bird", "Coco")
 
 dog1.make_sound()
 cat1.make_sound()
 bird1.make_sound()
+
+dog2.make_sound()
+bird2.make_sound()
